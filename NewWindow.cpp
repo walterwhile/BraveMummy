5ff5fa8623a9b5d92a54aea02ea71650f7799153
Ilsidena <ilsidena@mail.ru>
2018-03-22 00:19:07 +0300

    Successful work with NewWindow.cpp

---
+++ NewWindow.cpp
@@ -0,0 +1,25 @@
+#include <GL/glut.h>
+
+void display()
+{
+  glClear(GL_COLOR_BUFFER_BIT); // cleaning of frame buffer
+  glutSwapBuffers(); // change background buffer and frame buffer 
+}
+
+int main(int argc, char **argv)
+{
+  glutInit(&argc, argv); // GLUT inicialization
+
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH); // settings
+
+  glutInitWindowSize(1024, 768);
+  glutInitWindowPosition(100, 100);
+  glutCreateWindow("Les1.Window"); // creation of window
+
+  glutDisplayFunc(display);
+
+  glutMainLoop(); // giving control to GLUT
+
+  return 0;
+}
+
